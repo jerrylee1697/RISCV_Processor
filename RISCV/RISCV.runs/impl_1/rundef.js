2a6ed40ee2e44d051d3eaef7ca7cbd9c513cf45d
Jerry <jerrylee1697@gmail.com>
2017-12-07 18:17:58 -0800

    Testing more instructions

--- RISCV/RISCV.runs/impl_1/rundef.js
+++ RISCV/RISCV.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log basys3_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source basys3_wrapper.tcl -notrace" );
 
--- RISCV/RISCV.runs/impl_1/runme.sh
+++ RISCV/RISCV.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log basys3_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source basys3_wrapper.tcl -notrace
 
 
